d24211b4e1c5f7896e731602690eafd0d32c882e
johnniesimpson <johnniesimpson@outlook.com>
2020-12-13 15:53:14 -0500

    Refactored column creating to nest in row creation. Added conditions for color-coding text box based on comparison between dayJS current time and timeblock value.

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,104 @@
-var currentDate = dayjs().format('dddd, MMMM DD YYYY');
+// Set default variables
+var currentDate = dayjs().format('dddd, DD MMMM YYYY');
 var currentHour = dayjs().format('h');
+var testHour = dayjs().hour();
 var currentMinute = dayjs().format('mm');
 var current12 = dayjs().format('A');
-var columnWidths = [2, 8, 2];
-var columnNumber = 0;
 
+// Display the current time and date
 $('#currentDay').text(currentHour + ":" + currentMinute + " " + currentDate);
 
 // Timeblock array holding an object for each workday hour
 var timeBlocks = [
     {
-        time: '9',
+        time: 8,
         status: '',
+        showTime: '8AM',
     },
     {
-        time: '10',
+        time: 9,
         status: '',
+        showTime: '9AM',
     },
     {
-        time: '11',
+        time: 10,
         status: '',
+        showTime: '10AM', 
     },
     {
-        time: '12',
+        time: 11,
         status: '',
+        showTime: '11AM',
     },
     {
-        time: '13',
+        time: 12,
         status: '',
+        showTime: '12PM',
     },
     {
-        time: '14',
+        time: 13,
         status: '',
+        showTime: '1PM',
     },
     {
-        time: '15',
+        time: 14,
         status: '',
+        showTime: '2PM',
     },
     {
-        time: '16',
+        time: 15,
         status: '',
+        showTime: '3PM',
     },
     {
-        time: '17',
+        time: 16,
         status: '',
+        showTime: '4PM',
+    },
+    {
+        time: 17,
+        status: '',
+        showTime: '5PM',
+    },
+    {
+        time: 18,
+        status: '',
+        showTime: '6PM',
     },
 ]
 
-// Create rows to hold each timeblock
+// Create rows to hold timeblock columns
 for (var i = 0; i < timeBlocks.length; i++) {
-    console.log(timeBlocks[i].time);
-    var row = $('<div>')
+    var row = $('<form>');
     row.attr('class', 'row');
     $('.container').append(row);
-}
-// Create columns nested within each timeblock row
-for (var i = 0; i < columnWidths.length; i++) {
-    var col = $('<div>');
-     if (columnNumber === 0) {
-        col.attr('class', "col-" + columnWidths[i]);
-        col.addClass('timeblock');
-        col.addClass('hour');
+    for (var p = 0; p < 3; p++) {
+        if (p === 0) {
+            var timeDisplay = $('<div>');
+            timeDisplay.text(timeBlocks[i].showTime);
+            timeDisplay.attr('class', 'col-2 time-block hour');
+        }
+        else if (p === 1) {
+            var planner = $('<textarea>');
+            planner.attr('class', 'col-8 description');
+            if (testHour === timeBlocks[i].time) {
+                $(planner).addClass('present');
+            }
+            else if (testHour < timeBlocks[i].time) {
+                $(planner).addClass('past');
+            }
+            else {
+                $(planner).addClass('future');
+            }
+        }
+        else if (p === 2) {
+            var save = $('<i>');
+            save.attr('class', 'col-2 saveBtn fas fa-save fa-4x fa-fw');
+        }
     }
-    if (columnNumber === 1) {
-        var col = $('<textarea>');
-        col.attr('class', 'col-' + columnWidths[i]);
-        col.addClass('description');
-        col.addClass('past');
-    }
-    if (columnNumber === 2) {
-        col = $('<i>');
-        col.attr('class', 'col-' + columnWidths[i]);
-        col.addClass('saveBtn');
-        col.addClass('fas fa-save fa-3x')
-    }
-    
-    $('.row').append(col);
-    columnNumber++
-   
+    // Send column detail to each row
+    $(row).append(timeDisplay, planner, save);
 }
 
-for (var i = 0; i < timeBlocks.length; i++) {
-    $('.timeblock').text(timeBlocks[i].time);
-}
+console.log(testHour);
+console.log(parseInt($('#id').val()))

d7009e75ae7bbdd86664fab41cc46d377bb00d64
johnniesimpson <johnniesimpson@outlook.com>
2020-12-12 21:27:20 -0500

    Created an array to hold timeblock objects. Experimenting with dayJS formatting.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,50 @@
+var currentDate = dayjs().format('dddd, MMMM DD YYYY');
+console.log(currentDate);
 
+var currentHour = dayjs().format('h');
 
-var currentTime = dayjs().format('dddd, MMMM DD');
-console.log(currentTime);
+var currentMinute = dayjs().format('mm');
 
-$('#currentDay').text(currentTime);
+var current12 = dayjs().format('A');
+
+$('#currentDay').text(currentHour + ":" + currentMinute + " " + currentDate);
+
+var timeBlocks = [
+    {
+        time: 9,
+        status: '',
+    },
+    {
+        time: 10,
+        status: '',
+    },
+    {
+        time: 11,
+        status: '',
+    },
+    {
+        time: 12,
+        status: '',
+    },
+    {
+        time: 13,
+        status: '',
+    },
+    {
+        time: 14,
+        status: '',
+    },
+    {
+        time: 15,
+        status: '',
+    },
+    {
+        time: 16,
+        status: '',
+    },
+    {
+        time: 17,
+        status: '',
+    },
+]
+

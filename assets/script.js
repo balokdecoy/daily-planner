5d3655bb481c1cbf8ba78966b91533598e8ea5f0
johnniesimpson <johnniesimpson@outlook.com>
2020-12-12 23:36:39 -0500

    Adding columns with unique classes and attributes dynamically.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 var currentDate = dayjs().format('dddd, MMMM DD YYYY');
-console.log(currentDate);
-
 var currentHour = dayjs().format('h');
-
 var currentMinute = dayjs().format('mm');
-
 var current12 = dayjs().format('A');
+var columnWidths = [2, 8, 2];
+var columnNumber = 0;
 
 $('#currentDay').text(currentHour + ":" + currentMinute + " " + currentDate);
 
+// Timeblock array holding an object for each workday hour
 var timeBlocks = [
     {
         time: 9,
@@ -48,17 +47,32 @@ var timeBlocks = [
     },
 ]
 
+// Create rows to hold each timeblock
 for (var i = 0; i < timeBlocks.length; i++) {
     var row = $('<div>')
     row.attr('class', 'row');
     $('.container').append(row);
 }
 
-var columnWidths = [2, 8, 2];
-
+// Create columns nested within each timeblock row
 for (var i = 0; i < columnWidths.length; i++) {
     var col = $('<div>');
-    col.attr('class', columnWidths[i]);
+     if (columnNumber === 0) {
+        col.attr('class', "col-" + columnWidths[i]);
+        col.addClass('timeblock');
+        col.addClass('hour');
+    }
+    if (columnNumber === 1) {
+        var col = $('<textarea>');
+        col.attr('class', 'col-' + columnWidths[i]);
+        col.addClass('description');
+        col.addClass('past');
+    }
+    if (columnNumber === 2) {
+        col.attr('class', 'col-' + columnWidths[i]);
+        col.addClass('saveBtn');
+    }
     $('.row').append(col);
+    columnNumber++
+   
 }
-

fad80dfb59b1b3b9bf6b7e4b5d674c1995d12f77
johnniesimpson <johnniesimpson@outlook.com>
2020-12-13 00:04:59 -0500

    Added save button Font Awesome.

--- assets/script.js
+++ assets/script.js
@@ -10,50 +10,50 @@ $('#currentDay').text(currentHour + ":" + currentMinute + " " + currentDate);
 // Timeblock array holding an object for each workday hour
 var timeBlocks = [
     {
-        time: 9,
+        time: '9',
         status: '',
     },
     {
-        time: 10,
+        time: '10',
         status: '',
     },
     {
-        time: 11,
+        time: '11',
         status: '',
     },
     {
-        time: 12,
+        time: '12',
         status: '',
     },
     {
-        time: 13,
+        time: '13',
         status: '',
     },
     {
-        time: 14,
+        time: '14',
         status: '',
     },
     {
-        time: 15,
+        time: '15',
         status: '',
     },
     {
-        time: 16,
+        time: '16',
         status: '',
     },
     {
-        time: 17,
+        time: '17',
         status: '',
     },
 ]
 
 // Create rows to hold each timeblock
 for (var i = 0; i < timeBlocks.length; i++) {
+    console.log(timeBlocks[i].time);
     var row = $('<div>')
     row.attr('class', 'row');
     $('.container').append(row);
 }
-
 // Create columns nested within each timeblock row
 for (var i = 0; i < columnWidths.length; i++) {
     var col = $('<div>');
@@ -69,10 +69,17 @@ for (var i = 0; i < columnWidths.length; i++) {
         col.addClass('past');
     }
     if (columnNumber === 2) {
+        col = $('<i>');
         col.attr('class', 'col-' + columnWidths[i]);
         col.addClass('saveBtn');
+        col.addClass('fas fa-save fa-3x')
     }
+    
     $('.row').append(col);
     columnNumber++
    
+}
+
+for (var i = 0; i < timeBlocks.length; i++) {
+    $('.timeblock').text(timeBlocks[i].time);
 }
